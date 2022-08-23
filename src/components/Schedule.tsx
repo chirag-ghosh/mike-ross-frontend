990a677267b976985b27c619788c5d7e40335c55
chirag-ghosh <cghosh828049@gmail.com>
2022-08-23 16:46:56 +0530

    upcoming dates;

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -2,7 +2,7 @@ import { Card, createStyles, ScrollArea } from "@mantine/core"
 import { Calendar } from "@mantine/dates"
 import axios from "axios"
 import moment from "moment"
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { BACKEND_URL } from "../constants"
 
 const useStyles = createStyles((theme) => ({
@@ -28,9 +28,28 @@ function CaseItem({caseDetails}: {caseDetails: any}) {
     )
 }
 
+function UpcomingItem({upcomingDetails, onClick}: {upcomingDetails: {date: string, count: number}, onClick: React.MouseEventHandler<HTMLDivElement>}) {
+    return(
+        <Card withBorder radius="md" className="upcoming-item" onClick={onClick}>
+            {upcomingDetails.count} hearing(s) on {upcomingDetails.date}
+        </Card>
+    )
+}
+
 function Schedule() {
     const [date, setDate] = useState<Date | null>(new Date())
     const [cases, setCases] = useState<any[]>([])
+    const [upcoming, setUpcoming] = useState<any[]>([])
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/case/upcoming`)
+            .then((response) => {
+                setUpcoming(response.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     useEffect(() => {
         axios.get(`${BACKEND_URL}/case?date=${moment(date).format("DD-MM-YYYY")}`)
@@ -44,15 +63,24 @@ function Schedule() {
 
     return(
         <div className="schedule">
-            <Card withBorder radius="md" className="calendar">
-                <Calendar
-                    value={date}
-                    onChange={setDate}
-                    styles={(theme) => ({
-                        day: {fontSize: theme.fontSizes.lg, height: 60, width: 70}
-                    })}
-                />
-            </Card>
+            <div className="calendar-wrapper">
+                <Card withBorder radius="md" className="calendar-card">
+                    <Calendar
+                        className="calendar"
+                        value={date}
+                        onChange={setDate}
+                        styles={(theme) => ({
+                            day: {fontSize: theme.fontSizes.lg, height: 60, width: 70}
+                        })}
+                    />
+                </Card>
+                <div className="upcoming-list">
+                    <h2>Hearings in the next 7 days</h2>
+                    <ScrollArea>
+                        {upcoming.map((upcomingDate) => <UpcomingItem upcomingDetails={upcomingDate} key={upcomingDate.date} onClick={() => setDate(moment(upcomingDate.date, "DD-MM-YYYY").toDate())}/>)}
+                    </ScrollArea>
+                </div>
+            </div>
             <div className="case-list">
                 <h2>Hearings on: {moment(date).format("DD-MM-YYYY")}</h2>
                 <ScrollArea>
